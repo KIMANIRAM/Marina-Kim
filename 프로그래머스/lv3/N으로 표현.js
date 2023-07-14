c87e9dd44a722bef90bdab7d7256ea66f521b0cd
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2023-07-15 00:48:20 +0900

    Update N으로 표현.js

--- 프로그래머스/lv3/N으로 표현.js
+++ 프로그래머스/lv3/N으로 표현.js
@@ -1,3 +1,4 @@
+// source: https://ha-young.github.io/2021/algorithm_javascript/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-N%EC%9C%BC%EB%A1%9C%ED%91%9C%ED%98%84_lv3_dp/
 function addMemo(n, d, N) {
     let results = new Set();
     results.add(Number(String(N).repeat(n)));
