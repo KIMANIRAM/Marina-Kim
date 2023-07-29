cf31baa4c22245bd289423c1634e5802cf2d9ce3
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2023-07-29 22:44:14 +0900

    Create 스킬트리.js

---
+++ 프로그래머스/lv2/스킬트리.js
@@ -0,0 +1,6 @@
+function solution(skill, skill_trees) {
+    const regex = new RegExp(`[^${skill}]`, 'g');
+    return skill_trees.map(v => v.replace(regex, ''))
+        .filter(v => skill.indexOf(v) === 0 || v === '')
+        .length;
+}
