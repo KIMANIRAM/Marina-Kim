6b36310f5fb71994e4a17f07593095e66b6c6334
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2023-12-24 00:09:27 +0900

    Update 두개뽑아서더하기.js

--- 프로그래머스/lv1/두개뽑아서더하기.js
+++ 프로그래머스/lv1/두개뽑아서더하기.js
@@ -5,15 +5,13 @@ function getCombinations(arr, selectNumber) {
     arr.forEach((fixed, index, origin) => {
         const rest = origin.slice(index + 1);
         const combinations = getCombinations(rest, selectNumber - 1);
-        const attached = combinations.map(combination => [fixed + combination]);
+        const attached = combinations.map(combination => fixed + combination);
         results.push(...attached);
     })
-    
-    return results;
+    return new Set(results);
 }
 
 function solution(numbers) {
     const result = getCombinations(numbers, 2);
-    
-    return result;
+    return [...result].sort((a, b) => a - b);
 }
