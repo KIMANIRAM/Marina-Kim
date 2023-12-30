93d98cfa36fea92ec976363acaf01df50e27115e
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2023-12-30 22:18:53 +0900

    Update 실패율.js

--- 프로그래머스/lv1/실패율.js
+++ 프로그래머스/lv1/실패율.js
@@ -1,3 +1,4 @@
+// 12.29 - 첫 번째 풀이
 function solution(N, stages) {
     const failures = []; 
 
@@ -11,4 +12,17 @@ function solution(N, stages) {
     const sortable = failures.sort(([, a], [, b]) => b - a).map(pair => pair[0]);
 
     return sortable;
+}
+
+// 12.30 - declarative programming 방식의 두 번째 풀이
+function solution(N, stages) { 
+    const failures = Array(N).fill().map((_, i) => {
+        const currentStage = i + 1;
+        const losers = stages.filter(stage => stage === currentStage).length;
+        const challengers = stages.filter(stage => stage >= currentStage).length;
+        // console.log(`${i + 1}번 스테이지 실패율: ${losers}/${challengers}`);
+        return [i + 1, losers / challengers];
+    });
+                                    
+    return failures.sort(([, a], [, b]) => b - a).map(failure => failure[0]);
 }
