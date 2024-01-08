693cdcface91d758183454f6a311cd74d9c395a9
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-01-09 00:52:06 +0900

    Update 햄버거만들기.js

--- 프로그래머스/lv1/햄버거만들기.js
+++ 프로그래머스/lv1/햄버거만들기.js
@@ -1,15 +1,37 @@
-function solution(ingredient) { // "211231231" -> 21231 -> 2 -> 2개
-    const count = (function wrapup(str, cnt) {
-        if(ingredient.length < 4) return cnt;
-        
-        if(!cnt && !str.includes('1231')) {
-            return 0;
-        }
+// 처음 풀이 -> 메모리 초과로 (55/100)점
+// 숫자 배열을 문자열로 변경 + 재귀 알고리즘 => 많은 메모리 필요
+function solution(ingredient) { 
+    // 211231231,0 -> 21231,1 -> 2,2 => 2
+    function wrapup(str, cnt = 0) {
+        if(str.length < 4) return cnt;
         
         if(str.includes('1231')) {
-            wrapup(str.replace('1231', ''), cnt + 1);
+            return wrapup(str.replace('1231', ''), ++cnt);
         }
-    })(ingredient.join(''), 0)
+        
+        return cnt;
+    }
     
+    return wrapup(ingredient.join(''));
+}
+
+// 2차 풀이: 스택 (100/100)점
+function solution(ingredient) { 
+    const check = [];
+    let count = 0;
+
+    ingredient.forEach(e => {
+        check.push(e);
+
+        if(check[check.length - 1] === 1 &&
+           check[check.length - 2] === 3 &&
+           check[check.length - 3] === 2 &&
+           check[check.length - 4] === 1
+          ) {
+            check.splice(-4);
+            count++;
+        }
+    });
+
     return count;
 }
