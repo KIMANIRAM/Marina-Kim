1b1bd9daad7fcd5198a3400409cd3250cfc31183
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-01-15 23:01:06 +0900

    Update 신고결과받기.js

--- 프로그래머스/lv1/신고결과받기.js
+++ 프로그래머스/lv1/신고결과받기.js
@@ -1,28 +1,17 @@
 function solution(id_list, report, k) {
-    const answer = id_list.reduce((map, id) => {
-        map.set(id, 0);
+    const filterdReport = Array.from(new Set(report)).map(pair => pair.split(' '));
+    
+    const reportedIdMap = filterdReport.reduce((map, [id1, id2]) => {
+        map.set(id2, map.get(id2) + 1 || 1);
         return map;
     }, new Map());
-
-    const idMap = id_list.reduce((map, id) => {
-        map.set(id, [0, []]);
-        return map;
-    }, new Map());
-
-    for(let i = 0; i < report.length; i++) {
-        const [a, b] = report[i].split(' ');
-        if(idMap.get(b)[1].includes(a)) continue;
-        // [신고당한 횟수, 신고한 사람]
-        idMap.set(b, [idMap.get(b)[0] + 1, [...idMap.get(b)[1], a]]);
-    }
-
-    idMap.forEach((v, key) => {
-        let cnt = 0;
-        if(v[0] >= k) {
-            v[1].forEach(id => answer.set(id, answer.get(id) + 1));
+    
+    const sendMailIdMap = filterdReport.reduce((map, [id1, id2]) => {
+        if(reportedIdMap.get(id2) >= k) {
+            map.set(id1, map.get(id1) + 1 || 1);
         }
-
-    });
-
-    return Array.from(answer).map(([id, cnt]) => cnt);
+        return map;
+    }, new Map());
+    
+    return id_list.map(id => sendMailIdMap.get(id) || 0);
 }
