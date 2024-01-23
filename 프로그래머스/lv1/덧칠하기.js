09017679e09e76e46e1ab3a7a5221e48fc45bec5
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-01-24 00:17:05 +0900

    Update 덧칠하기.js

--- 프로그래머스/lv1/덧칠하기.js
+++ 프로그래머스/lv1/덧칠하기.js
@@ -1,18 +1,12 @@
-function solution(n, m, section) { // n=8, m=5, s=[2,3,6] -> 답: 1
-    let [start, end] = [0, 0];
+function solution(n, m, section) { 
+    let start = section[0];
     let cnt = 0;
     
-    for(let i = 1; i <= n - m + 1; i++) {
-        [start, end] = [i, m + i - 1];
-        let _cnt = 0;
-
-        for(let j = 0; j < section.length; j++) {
-            if(section[j] >= start && section[j] <= end) {
-                _cnt += 1;
-            } else break;
-        }
-        cnt = Math.max(cnt, _cnt);
-    }
+    for(let i = 0; i < section.length; i++){
+        if(start + m > section[i]) continue;
+        start = section[i];
+        cnt += 1;
+    };
     
-    return cnt;
+    return cnt + 1;
 }
