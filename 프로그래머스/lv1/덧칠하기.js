2645208f7d3057f610298c589671b66eefabcbef
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-01-24 00:27:29 +0900

    Update 덧칠하기.js

--- 프로그래머스/lv1/덧칠하기.js
+++ 프로그래머스/lv1/덧칠하기.js
@@ -1,12 +1,12 @@
 function solution(n, m, section) { 
-    let start = section[0];
-    let cnt = 0;
+    let start = section[0]; // 칠한 범위: start~start+m-1, 다음에 칠할 범위: start+m~
+    let cnt = 1;
     
     for(let i = 0; i < section.length; i++){
-        if(start + m > section[i]) continue;
+        if(section[i] < start + m) continue;
         start = section[i];
         cnt += 1;
     };
     
-    return cnt + 1;
+    return cnt;
 }
