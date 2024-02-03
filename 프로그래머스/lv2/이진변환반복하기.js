47c4870f4c9283d89c07cfb13bfbbc36afba98b1
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-02-03 23:37:04 +0900

    Update 이진변환반복하기.js
    순수 함수로 리팩토링

--- 프로그래머스/lv2/이진변환반복하기.js
+++ 프로그래머스/lv2/이진변환반복하기.js
@@ -1,17 +1,18 @@
 function solution(s) {
-    let result = s;
-    let [cvtCnt, zeroCnt] = [0, 0];
-    
-    while(result !== '1') {
-        if(result === '1') break;
-
-        result = [...result].filter(str => {
-            if(str === '1') return true;
+    const deleteZero = (binaryArr, zeroCnt) => {
+        const deleted = [...binaryArr].filter(binary => {
+            if(binary === '1') return true;
             zeroCnt++;
             return false;
         });
-        result = ((result.length).toString(2)).toString();
-        cvtCnt++;
-    }
-    return [cvtCnt, zeroCnt];
+        return [deleted.length, zeroCnt];
+    };
+    
+    const toBinary = (result, cvtCnt, zeroCnt) => {
+        if(result === '1') return [cvtCnt, zeroCnt];
+        const [c, _zeroCnt] = deleteZero([...result], zeroCnt);
+        return toBinary((c.toString(2)).toString(), cvtCnt + 1, _zeroCnt)
+    };
+    
+    return toBinary(s, 0, 0);
 }
