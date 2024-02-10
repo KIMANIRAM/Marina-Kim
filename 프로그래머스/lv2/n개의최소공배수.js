2d4303834ec37afecdd5ec3ebb1df79e56adf559
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-02-11 00:53:15 +0900

    Update n개의최소공배수.js
    resolved with recursive function

--- 프로그래머스/lv2/n개의최소공배수.js
+++ 프로그래머스/lv2/n개의최소공배수.js
@@ -1,14 +1,17 @@
 function solution(arr) {
     arr.sort((a, b) => a - b);
-    
+
     const GCD = (min, max) => max % min === 0 ? min : GCD(max % min, min);
     const LCM = (min, max) => min * max / GCD(min, max);
     
-    let temp = arr[0];
+    const getExtendLCM = (nums) => {
+        if(nums.length === 1) return nums[0];
+        const max = nums.pop();
+        const min = nums.pop();
+        const lcm = LCM(min, max);
+        nums.push(lcm);
+        return getExtendLCM(nums);
+    };
     
-    for(let i = 0; i < arr.length - 1; i++) {
-        temp = LCM(temp, arr[i + 1]);
-    }
-    
-    return temp;
+    return getExtendLCM(arr);
 }
