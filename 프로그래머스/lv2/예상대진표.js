c5d9cb83c9ecaad3afff7afd690beb241ad941bf
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-02-13 20:52:23 +0900

    Update 예상대진표.js
    resolved with binary search

--- 프로그래머스/lv2/예상대진표.js
+++ 프로그래머스/lv2/예상대진표.js
@@ -1,11 +1,23 @@
 function solution(n,a,b) {
-    let rnd = 0;
-    
-    while(a !== b) {
-        a = Math.ceil(a / 2);
-        b = Math.ceil(b / 2);
-        rnd += 1;
+    const [A, B] = [Math.min(a, b), Math.max(a, b)];
+
+    let round = Math.log2(n);
+    let left = 1;
+    let right = n;
+    let mid = Math.floor((left + right) / 2);
+
+    while(true) {
+        if(mid >= A && mid < B) break;
+        else if(mid <= A) {
+            left = mid;
+            mid = Math.floor((left + right) / 2);
+            round--;
+        } else if(mid >= B) {
+            right = mid;
+            mid = Math.floor((left + right) / 2);
+            round--;
+        }
     }
     
-    return rnd;
+    return round;
 }
