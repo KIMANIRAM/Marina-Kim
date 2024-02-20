c76b7fb9ed9fd5e4be926298e1df50eaf1931c83
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-02-20 23:56:53 +0900

    Update 기능개발.js
    resolved with two pointer

--- 프로그래머스/lv2/기능개발.js
+++ 프로그래머스/lv2/기능개발.js
@@ -1,21 +1,24 @@
 function solution(progresses, speeds) {
-    const answer = [0];
-    const days = progresses.map((progress, i) => {
-        const remainder = 100 - progress;
-        const day = remainder % speeds[i] ?
-              ~~(remainder / speeds[i]) + 1 : ~~(remainder / speeds[i]);
-        return day;
-    });
-    let maxDay = days[0];
-
-    for(let i = 0, j = 0; i < days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
+    const answer = [];
+    
+    while(progresses.length) {
+        for(let i = 0; i < progresses.length; i++) {
+            progresses[i] += speeds[i];
+        }
+        
+        if(progresses[0] >= 100){
+            let cnt = 0;
+            while(progresses.length){
+                if(progresses[0] >= 100){
+                    progresses.shift();
+                    speeds.shift();
+                    cnt += 1;
+                } else {
+                    break;
+                }
+            }
+            answer.push(cnt);
         }
     }
-    
-    return answer[0] === 0 ? [1] : answer;
+    return answer;     
 }
