85cc3c5e7625cd6eea2d0020252d15fdb4ea1b74
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-02-22 21:26:54 +0900

    Update 피로도.js
    resolved with backtracking

--- 프로그래머스/lv2/피로도.js
+++ 프로그래머스/lv2/피로도.js
@@ -1,23 +1,24 @@
 // 순열 활용 - 조합과는 달리 백트래킹 과정에서 방문한 노드는 탐색하지 않아야 함.
 function solution(k, dungeons) {
-    const n = dungeons.length;
-    const visited = Array.from({length: n}, () => false);
+    const N = dungeons.length;
     let max = 0;
-    
-    const DFS = (depth, val) => {
-        for(let i = 0; i < n; i++) {
-            if(!visited[i] && dungeons[i][0] <= val) {
-                visited[i] = true;
-                
-                DFS(depth + 1, val - dungeons[i][1]);
-                
-                visited[i] = false;
-            }
+
+    const dfs_backtracking = (curHp, visited, cnt) => {
+        for(let i = 0; i < N; i++) {
+            if(visited[i] || curHp < dungeons[i][0]) continue;
+            
+            const [req, drain] = dungeons[i];
+            const nextHp = curHp - drain;
+            const nextCnt = cnt + 1;
+            
+            visited[i] = true;
+            max = max < nextCnt ? nextCnt : max;
+            dfs_backtracking(nextHp, visited, nextCnt);
+            visited[i] = false;
         }
-        max = depth > max ? depth : max;
     }
-    
-    DFS(0, k);
+
+    dfs_backtracking(k, Array.from({ length: N }, () => false), 0);
     
     return max;
 }
