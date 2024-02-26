176ae8da9ee1a2ffc91efa915cfbbafe17dc7672
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-02-27 00:05:04 +0900

    Update 뉴스-클러스터링.js
    edit code

--- 프로그래머스/lv2/뉴스-클러스터링.js
+++ 프로그래머스/lv2/뉴스-클러스터링.js
@@ -1,12 +1,20 @@
-function solution(str1, str2) {
+function createMultiSet(str) {
+    const multiSet = [];
     const pattern = /[a-zA-Z]/;
-    
+    for(let i = 0; i < str.length - 1; i++) {
+        if(pattern.test(str[i]) && pattern.test(str[i + 1])) {
+            multiSet.push((str[i] + str[i + 1]).toLowerCase());
+        }
+    }
+    return multiSet;
+}
+
+function solution(str1, str2) {
     const a = createMultiSet(str1);
     const b = createMultiSet(str2);
     const set = Array.from(new Set([...a, ...b]));
     
     let [inter, uni] = [0, 0];
-    let similar = 1;
     
     for(let i = 0; i < set.length; i++) { 
         const checkedA = a.filter(e => e === set[i]).length;
