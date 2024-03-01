79f83ee90979c60fad963064ddf01640fe255553
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-03-01 12:26:26 +0900

    Update 스킬트리.js
    resolved hashmap

--- 프로그래머스/lv2/스킬트리.js
+++ 프로그래머스/lv2/스킬트리.js
@@ -1,6 +1,19 @@
 function solution(skill, skill_trees) {
-    const regex = new RegExp(`[^${skill}]`, 'g');
-    return skill_trees.map(v => v.replace(regex, ''))
-        .filter(v => skill.indexOf(v) === 0 || v === '')
-        .length;
+    const skillmap = [...skill].reduce((map, e) => {
+        map.set(e, e);
+        return map;
+    }, new Map());
+    
+    const skillset = [...skill].reduce((map, _, i) => {
+        map.set(skill.slice(0, skill.length - i), '');
+        return map;
+    }, new Map());
+    
+    const isIncludeSkill = (char) => skillmap.has(char);
+    const filterdSkillTree = skill_trees.map(str => [...str].filter(e => isIncludeSkill(e)).join('')); 
+    
+    return filterdSkillTree.reduce((cnt, str) => {
+        if(skillset.has(str) || str === '') return cnt + 1;
+        return cnt;
+    }, 0);
 }
