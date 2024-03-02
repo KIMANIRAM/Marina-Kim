ecc4604f4b1ab802c3aefa78f7146f7b9496ab13
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-03-03 03:45:36 +0900

    Update 방문길이.js
    resolved with set and map

--- 프로그래머스/lv2/방문길이.js
+++ 프로그래머스/lv2/방문길이.js
@@ -1,4 +1,30 @@
 function solution(dirs) {
-    var answer = 0;
-    return answer;
+    const visited = new Set();
+    const moveStrategy = new Map([
+        ['D', [-1, 0]],
+        ['R', [0, 1]],
+        ['U', [1, 0]],
+        ['L', [0, -1]],
+    ]);
+    
+    let [cx, cy] = [0, 0]; 
+    let cnt = 0;
+
+    for(let i = 0; i < dirs.length; i++) {
+        let [dx, dy] = moveStrategy.get(dirs[i]);
+        let [nx, ny] = [cx + dx, cy + dy];
+
+        if(nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
+        const prev = [cx, cy].join('');
+        const next = [nx, ny].join('');
+        // console.log(`이전좌표: ${cx},${cy},다음좌표: ${nx}, ${ny}`)
+        if(!visited.has(prev + next) || !visited.has(next + prev)) {
+            cnt++;
+            visited.add(prev + next);
+            visited.add(next + prev);
+        } 
+        [cx, cy] = [nx, ny]; 
+    }
+
+    return cnt;
 }
