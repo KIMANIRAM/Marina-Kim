ef9d9f5b23ea86a2e9ae35f65ff6853105391db3
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-03-11 00:52:07 +0900

    Update 전력망을-둘로-나누기.js
    resolved to short code

--- 프로그래머스/lv2/전력망을-둘로-나누기.js
+++ 프로그래머스/lv2/전력망을-둘로-나누기.js
@@ -11,35 +11,25 @@ function unionParent(parent, a, b) {
 }
 
 function solution(n, wires) {
+    const wireLen = wires.length;
+    const parentLen = n + 1;
     let result = Number.MAX_SAFE_INTEGER;
     
-    for(let i = 0; i < n; i++) {
-        const parent = Array(n).fill(0);
-        const findedParent = [];
-        for(let i = 1; i <= n; i++) {
-            parent[i] = i;
-        }
+    for(let i = 0; i < wireLen; i++) {
+        // i = 1부터 n까지 원소를 가지는 배열 초기화
+        const parent = Array.from({length: parentLen}, (e, i) => i);
         
-        for(let j = 0; j < n; j++) {
+        for(let j = 0; j < wireLen; j++) {
             if(i === j) continue;
-            const [a, b] = wires[i];
-            unionParent(parent, a, b)
+            const [a, b] = wires[j];
+            unionParent(parent, a, b);
         }
         
-        for(let j = 1; i < parent.length; i++) {
-            findedParent.push(findParent(parent, i));
-        }
-        // [1,1,1,1,2,2,2] => {1: 4개, 2: 3개} => [4, 3]
-        const counter = findedParent.reduce((map, root) => {
-            map.set(root, (map.get(root) || 0) + 1);
-            return map;
-        }, new Map());
-        
-        const counterToArr = Array.from(counter).map(e => e[1]);
-        const n1 = Math.max(...counterToArr);
-        const n2 = n - n1;
-        
-        if(result > Math.abs(n1 - n2)) result = Math.abs(n1 - n2);
+        // rootCnt: 루트노드를 부모로 가지는 노드의 수 ex. [0,1,1,1,1,2,2,2] => 4개
+        // 반대 그룹은 항상 전체 노드의 수(n)에서 rootCnt를 뺀 값 만큼 차이
+        const rootCnt = parent.slice(1).filter(x => parent[1] === findParent(parent, x)).length;
+        const restCnt = n - rootCnt;
+        result = Math.min(result, Math.abs(rootCnt - restCnt))
     }
     
     return result;
