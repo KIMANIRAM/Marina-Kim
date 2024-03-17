9d9606e0525e060a828f6ca02f7d18d95535138a
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-03-17 22:42:00 +0900

    Update 이중우선순위큐.js
    이중 우선순위큐는 set으로 간단하게 구현할 수 있음

--- 프로그래머스/lv3/이중우선순위큐.js
+++ 프로그래머스/lv3/이중우선순위큐.js
@@ -1,103 +1,57 @@
-class PriorityQueue {
+class DPQ {
     constructor() {
-        this.heap = [];
+        this.s = new Set()
     }
-
-    empty() {
-        return this.heap.length === 0;
+    
+    size() {
+        return this.s.size
     }
-
-    peek() {
-        if(this.empty()) return 0;
-        return this.heap[0];
+    
+    isEmpty() {
+        return this.s.size === 0
     }
-
-    push(data) {
-        this.heap.push(data);
-        let i = this.heap.length - 1;
-
-        while (i > 0) {
-            const parent = ~~((i - 1) / 2);
-            if (this.heap[parent] <= this.heap[i]) break;
-            [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]];
-            i = parent;
-        }
+    
+    insert(value) {
+        this.s.add(value)
     }
-
-    pop(data) {
-        if (this.empty()) return;
-
-        let value = 0;
-        if(data < 0) {
-            value = this.peek();
-            [this.heap[0], this.heap[this.heap.length - 1]] = [this.heap[this.heap.length - 1], this.heap[0]];
-        } else {
-            const maxIdx = this.findMaxIdx();
-            value = this.heap[maxIdx];
-            [this.heap[maxIdx], this.heap[this.heap.length - 1]] = [this.heap[this.heap.length - 1], this.heap[maxIdx]];
-        }
-
-        this.heap.pop();
-        this._heapify();
-
-        return value;
+    
+    getMin() {
+        return Math.min(...Array.from(this.s.values()))
     }
-
-    findMaxIdx() {
-        // leafNodes: 가장 마지막 index의 부모노드 + 1 부터 마지막 노드까지
-        if(this.heap.length === 1) return 0;
-
-        const n = this.heap.length - 1;
-        const parent = ~~((n - 1) / 2);
-        const leafNodes = this.heap.slice(parent + 1);
-        const maxValue = Math.max(...leafNodes);
-
-        return this.heap.findIndex(v => v === maxValue);
+    
+    getMax() {
+        return Math.max(...Array.from(this.s.values()))
     }
-
-    findMaxValue() {
-        if(this.empty()) return 0;
-        if(this.heap.length === 1) return this.peek();
-
-        const n = this.heap.length - 1;
-        const parent = ~~((n - 1) / 2);
-        const leafNodes = this.heap.slice(parent + 1);
-        const maxValue = Math.max(...leafNodes);
-
-        return maxValue;
+    
+    deleteMin() {
+        if(this.s.size === 0) return
+        this.s.delete(this.getMin())
     }
-
-    _heapify() {
-        const x = this.peek();
-        const n = this.heap.length;
-        let cur = 0;
-
-        while (2 * cur + 1 < n) {
-            const leftChild = 2 * cur + 1;
-            const rightChild = leftChild + 1;
-            const smallerChild = rightChild < n && this.heap[rightChild] < this.heap[leftChild] ? rightChild : leftChild;
-
-            if (x > this.heap[smallerChild]) {
-                [this.heap[cur], this.heap[smallerChild]] = [this.heap[smallerChild], this.heap[cur]];
-                cur = smallerChild;
-            } else {
-                break;
-            }
-        }
+    
+    deleteMax() {
+        if(this.s.size === 0) return
+        this.s.delete(this.getMax())
     }
 }
 
 function solution(operations) {
-    const pq = new PriorityQueue();
-
-    for(const oper of operations) {
-        const data = oper.substring(2) * 1;
-        if(oper[0] === 'I') {
-            pq.push(data);
+    const heap = new DPQ();
+    
+    for(let i = 0; i < operations.length; i++) {
+        const [command, value] = operations[i].split(' ');
+        if(command === 'D') {
+            if(heap.isEmpty()) {
+                continue;
+            }
+            if(value === '-1') {
+                heap.deleteMin();
+            } else {
+                heap.deleteMax();
+            }
         } else {
-            pq.pop(data);
+            heap.insert(+value);
         }
     }
-
-    return [pq.findMaxValue(), pq.peek()];
+    
+    return heap.isEmpty() ? [0, 0] : [heap.getMax(), heap.getMin()];
 }
