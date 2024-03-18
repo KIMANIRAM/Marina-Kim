0fc3e87fd77ae1969348342cf2118c070ca5b8b3
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-03-18 22:48:28 +0900

    Update 뒤에있는큰수.js
    resolved using 2d array

--- 프로그래머스/lv2/뒤에있는큰수.js
+++ 프로그래머스/lv2/뒤에있는큰수.js
@@ -1,15 +1,15 @@
 // 스택 또는 큐를 사용하는 문제 - 일단 모아놓고 조건에 맞으면 한꺼번에 제거
-function solution(numbers) { 
-    const st = [0]; // 큰수찾기를 해야 할 숫자의 인덱스
-    const answer = Array(numbers.length).fill(-1);
+function solution(numbers) {
+    const st = []; // [[0,9], [1,1], [6,4], ... [idx, element]]
+    let result = Array(numbers.length).fill(-1);
     
-    for(let i = 1; i < numbers.length; i++) {
-        const next = numbers[i];
-        while(st.length && numbers[st[st.length - 1]] < next) {
-            answer[st.pop()] = next;
-        }   
-        st.push(i); 
+    for(let i = 0; i < numbers.length; i++) {
+        while(st.length && st[st.length - 1][1] < numbers[i]) {
+            const [idx, num] = st.pop();
+            result[idx] = numbers[i];
+        }
+        st.push([i, numbers[i]])
     }
-    
-    return answer;
+
+    return result;
 }
