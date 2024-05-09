9d851f2d86bfc14788b95136aee2e50676b25eaa
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-05-10 00:08:45 +0900

    Update 멀쩡한-사각형.js
    solved with GCD

--- 프로그래머스/lv2/멀쩡한-사각형.js
+++ 프로그래머스/lv2/멀쩡한-사각형.js
@@ -1,3 +1,12 @@
+// 수학 유형의 문제. 경우의 수를 1부터 나열해서 규칙을 찾아서 풀어본다.
 function solution(w, h) {
+    const getGCD = (min, max) => max % min === 0 ? min : getGCD(max % min, min);
     
+    const squaresInCrossArea = (min, max) => {
+        const gcd = getGCD(min, max);
+        // console.log(min, max, gcd, Math.floor(min / gcd) + Math.floor(max / gcd) - 1)
+        return (Math.floor(min / gcd) + Math.floor(max / gcd) - 1) * gcd;
+    }
+    
+    return w * h - squaresInCrossArea(Math.min(w, h), Math.max(w, h));
 }
