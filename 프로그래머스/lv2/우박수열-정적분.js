3d3475feb94a86dc5372c27ba963ee506f325dcb
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-05-10 10:56:37 +0900

    Update 우박수열-정적분.js
    exception handling

--- 프로그래머스/lv2/우박수열-정적분.js
+++ 프로그래머스/lv2/우박수열-정적분.js
@@ -1,7 +1,7 @@
 function getCollatz(kArr) {
     if(kArr[kArr.length - 1] === 1) return kArr;
-    if(kArr[kArr.length - 1] % 2 === 0) return [...kArr, kArr[kArr.length - 1] / 2];
-    return [...kArr, kArr[kArr.length - 1] * 3 + 1];
+    if(kArr[kArr.length - 1] % 2 === 0) return getCollatz([...kArr, kArr[kArr.length - 1] / 2]);
+    return getCollatz([...kArr, kArr[kArr.length - 1] * 3 + 1]);
 }
 
 function getSize(s) {
@@ -19,21 +19,27 @@ function getPrefixSum(integrals) {
         sum += integrals[i];
         result.push(sum);
     }
+    return result;
 }
 
 function solution(k, ranges) {
     // 1. sequence 배열 구하기, [5, 16, 8, 4, 2, 1]
     const sequence = getCollatz([k]);
+    // console.log(sequence)
     // 2. 정적분 크기 배열 integrals구하기, [10.5, 12.0, 6.0, 3.0, 1.5]
     const integrals = getSize(sequence);
+    // console.log(integrals)
     // 3. prefixSum 배열 구하기
     const prefixSum = getPrefixSum(integrals);
     // 4. 쿼리에 맞는 구간합을 구하기, p[right] - [left - 1]
+    console.log(prefixSum)
     const result = ranges.map(query => {
-        const [left, right] = [query[0], k + query[1]];
-        if(left === right) return 0;
-        if(left < right) return -1;
-        return prefixSum[right] - prefixSum[left - 1];
+        // console.log(query)
+        const [left, right] = [query[0], prefixSum.length + query[1] - 1];
+        // console.log(left, right, prefixSum[right] - prefixSum[left])
+        if(left === right) return (0).toFixed(1);
+        if(left > right) return (-1).toFixed(1);
+        return (prefixSum[right] - prefixSum[left]).toFixed(1);
     });
     
     return result;
