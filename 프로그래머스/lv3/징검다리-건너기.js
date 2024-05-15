5a20301b240113540a7297bda96e00bd67baebfd
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-05-15 20:48:39 +0900

    Update 징검다리-건너기.js
    resolved with bitwise

--- 프로그래머스/lv3/징검다리-건너기.js
+++ 프로그래머스/lv3/징검다리-건너기.js
@@ -1,7 +1,7 @@
-function build(arr, n) {
+function createSegmentTree(n, arr) {
     const K = Math.ceil(Math.log2(n));
     const start = Math.pow(2, K);
-    const size = start * 2;
+    const size = start << 1;
     const ST = Array(size).fill(0);
     
     for(let i = 0; i < n; i++) {
@@ -9,38 +9,39 @@ function build(arr, n) {
     }
     
     for(let i = start - 1; i > 0; i--) {
-        ST[i] = Math.max(ST[i * 2], ST[i * 2 + 1]);
+        ST[i] = Math.max(ST[i << 1], ST[i << 1 | 1]);
     }
     
     return [K, ST];
 }
 
-function query(K, ST, min, max) {
+function query(k, st, min, max) {
+    let start = min + Math.pow(2, k);
+    let end = max + Math.pow(2, k);
     let result = 0;
-    let start = min + Math.pow(2, K);
-    let end = max + Math.pow(2, K);
     
     while(start <= end) {
         if(start % 2 === 1) {
-            result = Math.max(result, ST[start]);
+            result = Math.max(result, st[start]);
         }
         if(end % 2 === 0) {
-            result = Math.max(result, ST[end]);
+            result = Math.max(result, st[end]);
         }
-        start = Math.floor((start + 1) / 2);
-        end = Math.floor((end - 1) / 2);
+        start = (start + 1) >> 1;
+        end = (end - 1) >> 1;
     }
-
+    
     return result;
 }
 
 function solution(stones, k) {
-    let answer = Number.MAX_SAFE_INTEGER;
-    const [K, ST] = build(stones, stones.length);
-    for(let i = 0; i <= stones.length - k; i++) {
-        const max = query(K, ST, i, i + k - 1); 
-        answer = Math.min(answer, max);
+    const N = stones.length;
+    const [K, ST] = createSegmentTree(N, stones);
+    let min = 2e9;
+
+    for(let i = 0; i <= N - k; i++) {
+        min = Math.min(min, query(K, ST, i, i + k - 1));
     }
     
-    return answer;
+    return min;
 }
