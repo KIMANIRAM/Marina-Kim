6433a846e7b4bdf9664db6bcbc029163c59c4554
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-05-17 14:42:00 +0900

    Update 아이템줍기.js
    resolve and refact

--- 프로그래머스/lv3/아이템줍기.js
+++ 프로그래머스/lv3/아이템줍기.js
@@ -1,40 +1,50 @@
-function solution(rectangle, characterX, characterY, itemX, itemY) {
-    const map = Array(103).fill().map(e => Array(103).fill(0));
-    const DR = rectangle.map(row => row.map(point => point * 2));
-    
-    DR.forEach(([x1, y1, x2, y2]) => {
-        for(let i = x1; i <= x2; i++) {
-            for(let j = y1; j <= y2; j++) {
-                if(i === x1 || i === x2 || j === y1 || j === y2) {
-                    if(map[i][j] === 1) continue;
-                    map[i][j] += 1;
-                } else {
-                    map[i][j] += 2;
-                }
-            }
-        }
-    });
-    
-    const [charX, charY] = [characterX * 2, characterY * 2];
-    const [iX, iY] = [itemX * 2, itemY * 2];
+function bfs(map, chx, chy, ix, iy) {
     const dx = [0, -1, 1, 0];
     const dy = [1, 0, 0, -1];
-    const q = [[charX, charY, 0]];
-    map[charX][charY] += 100;
-    
+    const q = [];
+    let ans = 0;
+
+    q.push([chx, chy, 0]);
+    map[chx][chy] += 100;
+
     while(q.length) {
         const [cx, cy, cnt] = q.shift();
-        if(cx === iX && cy === iY) return cnt / 2;
-        
+        if(cx === ix && cy === iy) {
+            ans = cnt / 2;
+            break;
+        }
+
         for(let i = 0; i < 4; i++) {
             const nx = cx + dx[i];
             const ny = cy + dy[i];
-            
-            if(nx < 0 || nx >= 101 || ny < 0 || ny >= 101) continue;
+            if(nx < 0 || nx > 100 || ny < 0 || ny > 100) continue;
             if(map[nx][ny] === 1) {
-                map[nx][ny] += 100;
                 q.push([nx, ny, cnt + 1]);
+                map[nx][ny] += 100;
             }
         }
     }
+
+    return ans;
+};
+
+function solution(rectangle, characterX, characterY, itemX, itemY) {
+    const map = Array(101).fill().map(e => Array(101).fill(0));
+    const DR = rectangle.map(rect => rect.map(point => point * 2));
+    
+    for(let k = 0; k < DR.length; k++) {
+        const [minX, minY, maxX, maxY] = DR[k];
+        for(let i = minX; i <= maxX; i++) {
+            for(let j = minY; j <= maxY; j++) {
+                if(i === minX || i === maxX || j === minY || j === maxY) {
+                    if(map[i][j] === 1) continue;
+                    map[i][j] += 1;
+                } else {
+                    map[i][j] = 2;
+                }
+            }
+        }
+    }
+    
+    return bfs(map, characterX * 2, characterY * 2, itemX * 2, itemY * 2);
 }
