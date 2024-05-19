f4ed297cecf83d1fa14d24a0d6e5f5784c863d9a
Marina Kim <119808319+marinarinarina@users.noreply.github.com>
2024-05-19 23:30:00 +0900

    Update 베스트앨범.js
    resolve with SOLID

--- 프로그래머스/lv3/베스트앨범.js
+++ 프로그래머스/lv3/베스트앨범.js
@@ -1,27 +1,36 @@
-function solution(genres, plays) {
+function createPlaylist(genres, plays) {
     const playlist = new Map();
-    const likes = new Map();
-    
     for(let i = 0; i < plays.length; i++) {
-        likes.set(genres[i], (likes.get(genres[i]) || 0) + plays[i]);
         playlist.set(genres[i], (playlist.get(genres[i]) || []).concat([[i, plays[i]]]));
     }
-    
-    const sortedLikes = Array.from(likes).sort((a, b) => b[1] - a[1]);
-    const sortedList = [];
-    for(let i = 0; i < sortedLikes.length; i++) {
-        const list = playlist.get(sortedLikes[i][0]);
-        list.sort((a, b) => {
-            if(a[1] < b[1]) return 1;
-            if(a[1] > b[1]) return -1;
-            return a[0] - b[0];
-        });
-        // console.log(list)
-        sortedList.push(...list.slice(0, 2).map(e => e[0]));
+    return playlist;
+}
+
+function createOrders(playlist) {
+    const orders = [];
+    for(const [genre, list] of playlist) {
+        const cnt = list.reduce((a, b) => a + b[1], 0);
+        orders.push([genre, cnt]);
     }
-    
-    //console.log(playlist)
-    //console.log(likes)
-    //console.log(sortedLikes)
-    return sortedList;
+    return orders;
+}
+
+function sortOrders(orders) {
+    return orders.sort((a, b) => b[1] - a[1]).map(order => order[0]);
+}
+
+function makeAlbum(orders, playlist) {
+    const result = [];
+    const sortPlaylist = list => list.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : b[1] - a[1]);
+    for(let i = 0; i < orders.length; i++) {
+        result.push(...sortPlaylist(playlist.get(orders[i][0])).slice(0, 2).map(e => e[0]));
+    }
+    return result;
+}
+
+function solution(genres, plays) {
+    const playlist = createPlaylist(genres, plays);
+    const orders = createOrders(playlist);
+    const sortedGenres = sortOrders(orders);
+    return makeAlbum(orders, playlist);
 }
